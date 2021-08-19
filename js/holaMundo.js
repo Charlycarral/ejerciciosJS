a684bb724e0af062b0b57268bd838e4e46df3c60
Charly <carlosjoaquincarral@gmail.com>
2021-08-19 17:35:44 -0300

    se agrego estructura de condicionales

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -5,31 +5,27 @@ de multiple linea
 */
 
 // mostrar un mensaje por consola
-console.log('hola comision 17')
+console.log("hola comision 17");
 
 // mostrar un mensaje en la pagina html
-document.write("Este es mi primer mensaje")
+document.write("Este es mi primer mensaje");
 
 // mostar una ventana emergente
 // alert("Esto es una alerta")
 
 // crear o declarar variables var - let - const
 
-let palabraBuscada = " hola"; 
+let palabraBuscada = " hola";
 
-document.write('<br>Variable palabra Buscada: '+ palabraBuscada );
+document.write("<br>Variable palabra Buscada: " + palabraBuscada);
 
 // modificar el valor de una variable
 palabraBuscada = "pagina de blog";
 
-
-document.write('<br>Variable palabra Buscada: '+ palabraBuscada );
+document.write("<br>Variable palabra Buscada: " + palabraBuscada);
 
 const comision = 17;
 
-document.write('<br>Constante comision: '+ comision );
+document.write("<br>Constante comision: " + comision);
 
-let luz = true
-
-
-
+let luz = true;
