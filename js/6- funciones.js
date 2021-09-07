f123a23b5e938305fcf127399576b78782c5f846
Charly <carlosjoaquincarral@gmail.com>
2021-09-07 20:21:48 -0300

    Se agregar todos los ejercicios del tp funciones falta el numero 3

--- js/6- funciones.js
+++ js/6- funciones.js
@@ -1,4 +1,4 @@
-// funciones
+// funciones sin parametros
 function aumentarPrecio() {
   console.log("prueba");
   let disney = 385;
@@ -12,14 +12,41 @@ function aumentarPrecio() {
 function actualizarPrecio(monto, producto, precio) {
   let calculo = precio + monto;
   document.write(
-    "<br>El producto " + producto + "tiene el precio total $" + calculo
+    "<br>El producto " + producto + " tiene el precio total $" + calculo
   );
 }
+
+// funciones que devuelve un valor
+// function convertirDolaresPesos(precioDolar){
+//   let total = precioDolar * 184;
+//   return total
+//   // jamas escribimos en esta zona, la linea de return es la ultima que se ejecuta
+// }
+
+// funciones en forma de flecha =>
+let convertirDolaresPesos = (precioDolar) => {
+  let total = precioDolar * 184;
+  return total;
+};
+
+let monto = convertirDolaresPesos;
 
 // invocar o llamar a una funcion
 aumentarPrecio();
 
-let coeficienteInflacion = parseInt(  prompt("Ingrese el monto total de la inflacion del mes"));
+let coeficienteInflacion = parseInt(
+  prompt("Ingrese el monto total de la inflacion del mes")
+);
 actualizarPrecio(coeficienteInflacion, "Teclado hyperx ", 9000);
 actualizarPrecio(coeficienteInflacion, "Teclado logitec ", 14000);
-actualizarPrecio('mouse logitec', 9000, 100);
+actualizarPrecio(" mouse logitec ", 9000, 100);
+
+let dolarBlue = parseInt(prompt("Ingrese el valor del dolar Blue"));
+let iphone = convertirDolaresPesos(1200);
+
+document.write(`<br> Producto Iphone - Precio $ ${iphone} (producto nuevo)`);
+document.write(
+  `<br> Producto PSS - Precio $ ${convertirDolaresPesos(500)} (producto nuevo)`
+);
+
+// document.write('<br> Producto Iphone - Precio $' + iphone + '(producto nuevo)');
--- js/tp3/tpArreglosFunciones.js
+++ js/tp3/tpArreglosFunciones.js
@@ -1,24 +1,135 @@
 //Arrays
 //1- Crear un array llamado meses y que almacene el nombre de los doce meses del año.
 //Mostrar por pantalla en forma de lista  los doce nombres del arreglo.
+//
+//let meses = [
+//  "Enero",
+//  "Febrero",
+//  "Marzo",
+//  "Abril",
+//  "Mayo",
+//  "Junio",
+//  "Julio",
+//  "Agosto",
+//  "Setiembre",
+//  "Octubre",
+//  "Noviembre",
+//  "Diciembre",
+//];
+//
+//for (let posicion = 0; posicion < meses.length; posicion++) {
+//  document.write("<br> Mes de " + meses[posicion]);
+//}
+//
+//
+//2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo,
+//cuando el usuario seleccione cancelar o ingrese el valor “0” se debe mostrar el arreglo generado,
+//luego realizar las siguientes acciones:
+//
+//Mostrar la longitud del arreglo.
+//Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+//Añade en última posición la ciudad de París.
+//Escribe por pantalla el elemento que ocupa la segunda posición.
+//Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
-let meses = [
-  "Enero",
-  "Febrero",
-  "Marzo",
-  "Abril",
-  "Mayo",
-  "Junio",
-  "Julio",
-  "Agosto",
-  "Setiembre",
-  "Octubre",
-  "Noviembre",
-  "Diciembre",
-];
+// let arrayCiudades = [];
 
-for (let posicion = 0; posicion < meses.length; posicion++) {
-  document.write("<br> Mes de " + meses[posicion]);
-}
+// do {
+//   let ciudadesUsuario = prompt("Ingrese nombre de ciudades");
+//   arrayCiudades.push(ciudadesUsuario);
+// } while (confirm("¿Desea Ingresar alguna ciudad mas?"));
 
+// document.write("Listado de ciudades tipeadas por el usuario: <br> ");
+// for (elemento = 0; elemento < arrayCiudades.length; elemento++) {
+//   document.write(
+//     "Ciudad " + (elemento + 1) + ": " + arrayCiudades[elemento] + "<br>"
+//   );
+// }
 
+// document.write("Longitud del Arreglo :" + arrayCiudades.length + "<br>");
+
+// document.write("Elemento en primer posicion: " + arrayCiudades[0] + "<br>");
+// document.write("Elemento en tercer posicion: " + arrayCiudades[2] + "<br>");
+// document.write(
+//   "Elemento en primer posicion: " +
+//     arrayCiudades[arrayCiudades.length - 1] +
+//     "<br>"
+// );
+
+// arrayCiudades.push("París");
+
+// console.log(arrayCiudades[1]);
+
+// arrayCiudades.splice(1, 1, "Barcelona");
+
+// console.log(arrayCiudades[1]);
+
+// 3- Escribir un script que simule el lanzamiento de dos dados.
+// Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos
+// de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma,
+// repitiendo 50 veces esta operación.
+
+//Completar
+
+// Funciones
+
+// 1- Escribir el código de una función a la que se pasa como parámetro un número entero y
+// devuelve como resultado una cadena de texto que indica si el número es par o impar.
+// Mostrar por pantalla el resultado devuelto por la función.
+
+// function numero(numeroUsuario) {
+//   if (numeroUsuario % 2 == 0) {
+//     alert("El numero seleccionado por el usuario es Par");
+//   } else {
+//     alert("El numero seleccionado por el usuario es Impar");
+//   }
+// }
+
+// numero(prompt("Ingrese un numero"))
+
+// 2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento.
+// A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas,
+// sólo por minúsculas o por una mezcla de ambas.
+
+// function textoParametro(texto) {
+//   if (texto == texto.toUpperCase()) {
+//     alert("El texto " + texto + " esta formado por Mayusculas");
+//   } else if (texto == texto.toLowerCase()) {
+//     alert("El texto " + texto + " esta formado por Minusculas");
+//   } else {
+//     alert("El texto " + texto + " tiene Mayusculas y Minusculas");
+//   }
+// }
+
+// textoParametro(prompt("Ingrese un texto"));
+
+// 3- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo,
+// luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+// La fórmula del perímetro  es p = 2*(a +b)
+
+// let ladoA = parseInt(prompt("Ingrese el valor del lado A de un rectangulo"));
+// let ladoB = parseInt(prompt("Ingrese el valor del lado B de un rectangulo"));
+
+// function valorPerimetro() {
+//   let perimetro = 2 * (ladoA + ladoB);
+//   document.write("El perimetro del rectangulo es " + perimetro);
+// }
+
+// valorPerimetro();
+
+// 4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla,
+// la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10
+// del número elegido por el usuario.
+
+// let numero = prompt("Ingrese un numero");
+
+// function tablaMultiplicar() {
+//   for (elemento = 1; elemento <= 10; elemento++) {
+//     document.write(
+//       numero + " x " + elemento + " = " + numero * elemento + "<br>"
+//     );
+//   }
+// }
+
+// document.write("Tabla de Multiplicar del "+ numero + '<br>');
+// tablaMultiplicar();
